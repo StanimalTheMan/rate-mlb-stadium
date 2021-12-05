12aea0f5776dc454ee4ca78d964a483c2ccbc772
StanimalTheMan <sjc591@nyu.edu>
2021-12-04 19:29:45 -0500

    naively add user profile endpoint

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -13,7 +13,30 @@ function getUserRoutes() {
   return router;
 }
 
-async function getProfile(req, res, next) {}
+async function getProfile(req, res, next) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.params.userId,
+    },
+  });
+
+  if (!user) {
+    return next({
+      message: `No user found with id: "${req.params.userId}"`,
+      statusCode: 404,
+    });
+  }
+
+  const reviews = prisma.review.findMany({
+    where: {
+      userId: req.params.userId,
+    },
+  });
+
+  user.reviews = reviews;
+
+  res.status(200).json({ user });
+}
 // async function getUserReviews(req, res, next) {
 //   const reviews = prisma.review.findMany({
 //     where: {
